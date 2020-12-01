5425dc6bdc76e3fcd581dc7632edcc3fd6d91be6
patsalcedo <patricia.salcedo15@outlook.com>
2020-12-01 14:48:05 +1300

    cleaned up test code

--- StringCalculator.Tests/CalculatorTest.cs
+++ StringCalculator.Tests/CalculatorTest.cs
@@ -15,9 +15,6 @@ namespace StringCalculator.Tests
         [Fact] // step 1
         public void Return_Zero_To_Empty_String()
         {
-            // GIVEN
-            // var calculator = new Calculator();
-
             // WHEN
             var actual = _calculator.Add("");
 
@@ -30,11 +27,8 @@ namespace StringCalculator.Tests
         [InlineData("3",3)]
         public void Return_Number_Given_Single_String_Number(string input, int expectedOutput)
         {
-            // GIVEN
-            var calculator = new Calculator();
-            
             // WHEN
-            var actual = calculator.Add(input);
+            var actual = _calculator.Add(input);
             
             // THEN
             Assert.Equal(expectedOutput, actual);
@@ -45,11 +39,8 @@ namespace StringCalculator.Tests
         [InlineData("3,5", 8)]
         public void Return_Sum_Of_Two_Number_Input(string input, int expectedOutput)
         {
-            // GIVEN
-            var calculator = new Calculator();
-            
             // WHEN
-            var actual = calculator.Add(input);
+            var actual = _calculator.Add(input);
 
             // THEN
             Assert.Equal(expectedOutput, actual);
@@ -60,11 +51,8 @@ namespace StringCalculator.Tests
         [InlineData("3,5,3,9",20)]
         public void Return_Sum_Of_Input_String_Numbers(string input, int expectedOutput)
         {
-            // GIVEN
-            var calculator = new Calculator();
-            
             // WHEN
-            var actual = calculator.Add(input);
+            var actual = _calculator.Add(input);
 
             // THEN
             Assert.Equal(expectedOutput, actual);
@@ -75,11 +63,8 @@ namespace StringCalculator.Tests
         [InlineData("3\n5\n3,9", 20)]
         public void Return_Sum_Of_Multiple_With_NewLine_And_Commas(string input, int expectedOutput)
         {
-            // GIVEN
-            var calculator = new Calculator();
-            
             // WHEN
-            var actual = calculator.Add(input);
+            var actual = _calculator.Add(input);
 
             // THEN
             Assert.Equal(expectedOutput, actual);
@@ -88,11 +73,8 @@ namespace StringCalculator.Tests
         [Fact] // step 6
         public void Support_Different_Delimiters()
         {
-            // GIVEN
-            var calculator = new Calculator();
-            
             // WHEN
-            var actual = calculator.Add("//;\n1;2");
+            var actual = _calculator.Add("//;\n1;2");
         
             // THEN
             Assert.Equal(3, actual);
@@ -101,22 +83,15 @@ namespace StringCalculator.Tests
         [Fact] // step 7
         public void Throw_Exception_For_Negatives()
         {
-            // GIVEN
-            var calculator = new Calculator();
-            
-            // THEN
-            var ex = Assert.ThrowsAny<Exception>(() => calculator.Add("-1,2,-3"));
-            Assert.Equal("Negatives not allowed: -1, -3",ex.Message);
+            var e = Assert.ThrowsAny<Exception>(() => _calculator.Add("-1,2,-3"));
+            Assert.Equal("Negatives not allowed: -1, -3",e.Message);
         }
 
         [Fact] // step 8
         public void Ignore_Numbers_Greater_Than_Thousand()
         {
-            // GIVEN
-            var calculator = new Calculator();
-            
             // WHEN
-            var actual = calculator.Add("1000,1001,2");
+            var actual = _calculator.Add("1000,1001,2");
         
             // THEN
             Assert.Equal(2, actual);
@@ -125,11 +100,8 @@ namespace StringCalculator.Tests
         [Fact] // step 9
         public void Allow_Delimiters_Of_Any_Length()
         {
-            // GIVEN
-            var calculator = new Calculator();
-            
             // WHEN
-            var actual = calculator.Add("//[***]\n1***2***3");
+            var actual = _calculator.Add("//[***]\n1***2***3");
         
             // THEN
             Assert.Equal(6, actual);
@@ -138,11 +110,8 @@ namespace StringCalculator.Tests
         [Fact] // step 10
         public void Allow_Multiple_Delimiters()
         {
-            // GIVEN
-            var calculator = new Calculator();
-            
             // WHEN
-            var actual = calculator.Add("//[*][%]\n1*2%3");
+            var actual = _calculator.Add("//[*][%]\n1*2%3");
         
             // THEN
             Assert.Equal(6, actual);
@@ -151,11 +120,8 @@ namespace StringCalculator.Tests
         [Fact] // step 11
         public void Allow_Varying_Length_Multiple_Delimiters()
         {
-            // GIVEN
-            var calculator = new Calculator();
-            
             // WHEN
-            var actual = calculator.Add("//[***][#][%]\n1***2#3%4");
+            var actual = _calculator.Add("//[***][#][%]\n1***2#3%4");
         
             // THEN
             Assert.Equal(10, actual);
@@ -164,11 +130,8 @@ namespace StringCalculator.Tests
         [Fact] // step 12
         public void Allow_Delimiters_With_Numbers_In_Them()
         {
-            // GIVEN
-            var calculator = new Calculator();
-            
             // WHEN
-            var actual = calculator.Add("//[*1*][%]\n1*1*2%3");
+            var actual = _calculator.Add("//[*1*][%]\n1*1*2%3");
         
             // THEN
             Assert.Equal(6, actual);
